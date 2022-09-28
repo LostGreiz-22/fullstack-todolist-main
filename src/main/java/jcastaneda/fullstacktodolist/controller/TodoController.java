c5ffd017eea97cdb097b39a0b54db4f392a28a2e
LostGreiz-22 <lnnnkojp@gmail.com>
2022-09-28 18:46:17 +0800

    Initial commit

---
+++ src/main/java/jcastaneda/fullstacktodolist/controller/TodoController.java
@@ -0,0 +1,40 @@
+package jcastaneda.fullstacktodolist.controller;
+
+import jcastaneda.fullstacktodolist.dto.TodoDTO;
+import jcastaneda.fullstacktodolist.model.Todo;
+import jcastaneda.fullstacktodolist.service.TodoService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/todo")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @PutMapping("/addTodo/{newTodo}")
+    public List<TodoDTO> addTodo(@PathVariable @NonNull String newTodo) {
+        return todoService.addTodo(newTodo);
+    }
+
+    @PostMapping("/updateTodo")
+    public List<TodoDTO> updateTodo(@RequestBody @NonNull Todo updatedTodo) {
+        return todoService.updateTodo(updatedTodo);
+    }
+
+    @DeleteMapping("/deleteTodo/{id}")
+    public List<TodoDTO> deleteTodo(@PathVariable UUID id) {
+        return todoService.deleteTodo(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<TodoDTO> getAllTodo() {
+        return todoService.getAllTodos();
+    }
+
+}
